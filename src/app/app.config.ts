8210f666aaa63c28466a1cf83b1465e03b60f159
Nikita Kisiura <nikitar156@gmail.com>
2024-09-23 17:23:26 -0400

    created retry interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,15 @@ import {
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { provideRouter } from "@angular/router";
 
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { retryInterceptor } from "@core/interceptors";
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([retryInterceptor])),
     provideRouter(routes),
   ],
 };
